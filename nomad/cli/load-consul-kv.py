2138df1874dc8cca9786a7f7ecfc5b6baf43732d
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2019-12-31 18:37:14 +0100

    update

--- nomad/cli/load-consul-kv.py
+++ nomad/cli/load-consul-kv.py
@@ -9,11 +9,13 @@ sys.path.append(app_dir)
 
 from consul_keys import consul_keys
 
+consul_ip = "172.17.0.2"
+# consul_ip = "localhost"
+
 # nomad_keys = json.loads(open("./cli/consul-variables.json").read())
 
 for prefix, variables in consul_keys.items():
     for key, value in variables.items():
-        # response = requests.put(f"http://localhost:8500/v1/kv/{prefix}{key}",
-        response = requests.put(f"http://172.17.0.2:8500/v1/kv/{prefix}{key}",
+        response = requests.put(f"http://{consul_ip}:8500/v1/kv/{prefix}{key}",
                                 data=str.encode(value))
         # print(response.text)
